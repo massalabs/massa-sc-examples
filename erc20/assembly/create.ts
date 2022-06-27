b1519b75b1e09f471679fe58d18acdd8f1e9ace1
Grégory Libert <Grgory Libert>
2022-06-27 10:54:26 +0200

    migrates to mscl-token#standard-token-implementation

--- erc20/assembly/create.ts
+++ erc20/assembly/create.ts
@@ -1,13 +1,13 @@
-//export * from '../node_modules/mscl-token/assembly/erc20/erc20';
+export * from '../node_modules/mscl-token/assembly/erc20/erc20';
 
 // For this example we just want to rename the token
 // Everything else will be kept as defined in the mscl-token/erc20 module
 
 /**
- * Overwrite name function with wanted functionality.
+ * Overwrites `name` function with wanted functionality.
  *
- * @param {string} _ - Unused but mandatory. See https://github.com/massalabs/massa-sc-std/issues/18
- * @return {string} - the new toke name.
+ * @param {string} _ - unused but mandatory. See https://github.com/massalabs/massa-sc-std/issues/18
+ * @return {string} - the new token name.
  */
 export function name(_:string): string {
   return 'Massa Example token';
--- erc20/assembly/use.ts
+++ erc20/assembly/use.ts
@@ -1,4 +1,8 @@
-import {fileToBase64, print, createSC, Address} from 'massa-sc-std/assembly/index';
+import {
+  fileToBase64,
+  print,
+  createSC,
+  Address} from 'massa-sc-std/assembly/index';
 import {TokenWrapper} from 'mscl-token/assembly/index';
 
 /**
@@ -7,55 +11,54 @@ import {TokenWrapper} from 'mscl-token/assembly/index';
  * A smart contract is a WebAssembly module read from a wasm file
  * and encoded into base64.
  * `create_sc` takes that smart contract, sends it to the blockchain
- * and returns it address.
+ * and returns its address.
  *
  * @return {Address} - Address of the smart contract loaded into the blockchain.
  */
 function loadSC(): Address {
-  print("A");
-  const b = fileToBase64('./build/erc20_create.wasm');
-  print("B");
-  const a = createSC(b);
-  print("C");
-  return a;
+  // Reads erc20_create.wasm file, encodes it in base64 and
+  //   sets the result to `bytecode` variable.
+  // There is a transformer that can do that, but it's not working
+  //   with assemblyscript 0.20 (see https://github.com/massalabs/massa-sc-library/pull/29).
+  // Therefore, this transformation is done at compilation time
+  //   (see asbuild:use script in package.json).
+  const bytecode = fileToBase64('./build/erc20_create.wasm');
+
+  // Adds a new smart contract to the ledger and returns its address.
+  return createSC(bytecode);
 }
 
 /**
  * Main function called at smart contract runtime.
  *
- * TODO: clarify what and how arguments are passed.
- * TODO: Clarify what are the impact of teh returned value.
- *
- * @param {string} _args - Arguments passed to main function.
+ * @param {string} _ - unused but mandatory. See https://github.com/massalabs/massa-sc-std/issues/18
  * @return {i32} - ?
  */
-export function main(_args: string): i32 {
-  print("1");
+export function main(_: string): i32 { // eslint-disable-line @typescript-eslint/no-unused-vars
   const scAddress = loadSC();
 
-  print("2");
+  // Our smart contract is an token following Massa's standard token interface.
+  // Smart contract author proposes a wrapped that makes interaction easier
+  // following the same approach as an FFI bridge.
   const coin = new TokenWrapper(scAddress);
-  print("3");
-  print(scAddress.toByteString());
 
+  // Thanks to the wrapper we can call high level function without
+  // having to handle serializations, deserializations and type conversions
   const coinName = coin.name();
-  print("4");
 
-  const coinTotalSUpply = coin.totalSupply();
-  print("4b");
+  const coinTotalSupply = coin.totalSupply();
 
-  const coinSymbol = coin.symbol();
-    print("5");
-  
   const bal = coin.balanceOf(
-    Address.fromByteString("A1MrqLgWq5XXDpTBH6fzXHUg7E8M5U2fYDAF3E1xnUSzyZuKpMh")
-    );
-    print("6");
+      Address.fromByteString(
+          'A1MrqLgWq5XXDpTBH6fzXHUg7E8M5U2fYDAF3E1xnUSzyZuKpMh'
+      )
+  );
 
- // print(scAddress.toByteString() + //' balance: ' + bal.value().toString() + 
-  //' of token: ' + coinName + ' total supply: ' + coin.symbol());
+  print(
+      'Smart contract addess: ' + scAddress.toByteString() + '\n' +
+      'Balance: ' + bal.value().toString() + '\n' +
+      'Token: ' + coinName + '\n' +
+      'Total supply: ' +coinTotalSupply.value().toString());
 
-  print("7");
-  
   return 0;
 }
