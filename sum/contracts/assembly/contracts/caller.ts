031111c50e68536c535e39f9668d57812fbe5326
Nathan Seva <thykof@protonmail.ch>
2023-01-27 16:53:28 +0000

    fixup

--- sum/contracts/assembly/contracts/caller.ts
+++ sum/contracts/assembly/contracts/caller.ts
@@ -3,8 +3,6 @@ import { Address, call, callerHasWriteAccess } from '@massalabs/massa-as-sdk';
 
 /**
  * This function is meant to be called only one time: when the contract is deployed.
- *
- * @param _ - not used
  */
 export function constructor(binaryArgs: StaticArray<u8>): StaticArray<u8> {
   // This line is important. It ensures that this function can't be called in the future.
