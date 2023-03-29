d328e29cf8debd8c4be3abe8c09b2dc6c23e8041
Ben <br@massa.net>
2023-03-29 14:56:34 +0200

    [fix] modify main helloWorld file to use constructor

--- helloworld/assembly/contracts/main.ts
+++ helloworld/assembly/contracts/main.ts
@@ -1,9 +1,19 @@
-import { generateEvent } from '@massalabs/massa-as-sdk';
+import { callerHasWriteAccess, generateEvent } from '@massalabs/massa-as-sdk';
 
-// The `main` function is the entry point for smart contracts in AssemblyScript.
-// It takes a static array of bytes as its argument, but in this case we're ignoring it since we don't need it.
-export function main(_: StaticArray<u8>): void {
+/**
+ * This function is meant to be called only one time: when the contract is deployed.
+ *
+ * @param _ - not used
+ */
+export function constructor(_: StaticArray<u8>): StaticArray<u8> {
+  // This line is important. It ensures that this function can't be called in the future.
+  // If you remove this check, someone could call your constructor function and reset your smart contract.
+  if (!callerHasWriteAccess()) {
+    return [];
+  }
+
   // The `generateEvent` function is used to emit an event on the blockchain.
   // In this case, we're emitting an event with the message "Hello, World!".
   generateEvent('Hello, World!');
+  return [];
 }
