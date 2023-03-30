bf30479165fa8111a22c8d493cc5aeb27cd4df80
Grégory Libert <gl@massa.net>
2023-03-30 16:34:44 +0200

    update helloworld project (#55)
    Updates helloworld code sample and add a daily CI
    ---------
    
    Co-authored-by: Ben <br@massa.net>
    Co-authored-by: Adrien LF <alf@massa.net>

---
+++ helloworld/assembly/contracts/main.ts
@@ -0,0 +1,18 @@
+import { callerHasWriteAccess, generateEvent } from '@massalabs/massa-as-sdk';
+
+/**
+ * This function is meant to be called only one time: when the contract is deployed.
+ *
+ * @param _ - not used
+ */
+export function constructor(_: StaticArray<u8>): void {
+  // This line is important. It ensures that this function can't be called in the future.
+  // If you remove this check, someone could call your constructor function and reset your smart contract.
+  if (!callerHasWriteAccess()) {
+    return;
+  }
+
+  // The `generateEvent` function is used to emit an event on the blockchain.
+  // In this case, we're emitting an event with the message "Hello, World!".
+  generateEvent('Hello, World!');
+}
--- helloworld/assembly/main.ts
+++
@@ -1,6 +0,0 @@
-// The entry file of your WebAssembly module.
-import { generateEvent } from '@massalabs/massa-as-sdk';
-
-export function main(): void {
-	generateEvent("Hello, World!");
-}
