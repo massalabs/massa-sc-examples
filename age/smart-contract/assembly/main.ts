24e9c78649de296c051e5fb6269e49a808d8bfdf
0xSwapFeeder <tristan.gremont@epitech.eu>
2023-04-05 13:14:59 +0200

    feature(age-exemple): updated age smart contract + added documentation + added a 'get_age' function

--- age/smart-contract/assembly/main.ts
+++ age/smart-contract/assembly/main.ts
@@ -1,17 +1,78 @@
 // The entry file of your WebAssembly module.
-import { Storage, Args, toBytes } from "@massalabs/massa-as-sdk";
+import { Args } from "@massalabs/as-types";
+import { Storage, generateEvent } from "@massalabs/massa-as-sdk";
 
-export function initialize(_args: StaticArray<u8>): void {
-    let age = new Args();
-    age.add(1 as u32);
-    Storage.set(toBytes("alice"), age.serialize());
+/**
+ * This function is the constructor, it is always called once on contract deployement.
+ * 
+ * In this case, the constructor initiate's the store by adding a 'person' entry with a default 'age' value.
+ * @remarks 
+ *  If the entry doesn't exist the person is created.
+ *  It also generates an event that indicates the changes that are made.
+ * 
+ * @param args The serialized arguments (unused).
+ * 
+ * @returns none
+ */
+
+export function constructor(_: StaticArray<u8>): StaticArray<u8> {
+    let age = new Args().add(1 as u32);
+    let name = new Args().add("alice");
+    
+    Storage.set(name.serialize(), age.serialize());
+    return [];
 }
 
-export function change_age(args: StaticArray<u8>): void {
+
+/**
+ * This functions changes the age of somone by a given name and a given new age value.
+ * 
+ * @remarks 
+ *  If the entry doesn't exist the person is created.
+ *  It also generates an event that indicates the changes that are made.
+ * 
+ * @param args The serialized arguments.
+ * 
+ * @returns none
+ */
+export function change_age(args: StaticArray<u8>): StaticArray<u8> {
     let args_deserialized = new Args(args);
-    let name = args_deserialized.nextString();
-    let age = args_deserialized.nextU32();
-    let age_encoded = new Args();
-    age_encoded.add(age);
-    Storage.set(toBytes(name), age_encoded.serialize());
+    
+    let name = args_deserialized.nextString().expect("Missing name argument.");
+    let age = args_deserialized.nextU32().expect("Missing age argument.");
+
+    let age_encoded = new Args().add(age).serialize();
+    let name_encoded = new Args().add(name).serialize();
+
+    Storage.set(name_encoded, age_encoded);
+
+    generateEvent("Changed age of '"+name+"' to '"+age.toString()+"'");
+    return [];
 }
+
+/**
+ * This functions retreives the age of somone by a given name.
+ * 
+ * @remarks 
+ *  If the entry doesn't exist the execution is aborted.
+ * 
+ * @param args The serialized arguments.
+ * 
+ * @returns The serialized 'age' found.
+ */
+export function get_age(args: StaticArray<u8>): StaticArray<u8> {
+  let args_deserialized = new Args(args);
+  let name = args_deserialized.nextString().expect("Missing name argument.");
+  let name_encoded = new Args().add(name).serialize();
+  
+  if (Storage.has(name_encoded)) {
+    let age = Storage.get(name_encoded);
+  
+    return new Args().add(age).serialize();
+  } else {
+    abort("No such person's age is stored.")
+    return [];
+  } 
+  return [];
+}
+
