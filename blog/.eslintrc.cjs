24fbe92496a8844bc83b6d96c9bff32ed5d6cbad
unknown <adriancik01@gmail.com>
2023-04-07 16:16:51 +0200

    fixed linting

--- blog/.eslintrc.cjs
+++ blog/.eslintrc.cjs
@@ -1,6 +1,5 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
+  extends: [
+    '@massalabs',
+  ],
 };
