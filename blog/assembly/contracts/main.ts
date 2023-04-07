8e9263b974cab2c290bbaf73d96f474e6378e0d6
unknown <adriancik01@gmail.com>
2023-04-07 14:29:52 +0200

    improved documentation

--- blog/assembly/contracts/main.ts
+++ blog/assembly/contracts/main.ts
@@ -13,30 +13,39 @@ export function constructor(_: StaticArray<u8>): StaticArray<u8> {
         return [];
       }
     var n_blog_posts = 0;
+    //Set the initial value of N_BLOG_POSTS to 0 in the storage of the contract
     Storage.set("N_BLOG_POSTS", n_blog_posts.toString());
+    //This function is used to emit an event to the blockchain
     generateEvent(`Blog initiated`);
     return [];
 }
 
 export function post(binaryArgs: StaticArray<u8>): void {
     const argsDeser = new Args(binaryArgs);
+    //Get the post from the arguments and check if it is valid (not null)
     const post = argsDeser
     .nextString()
     .expect('Post argument is missing or invalid');
     var n_blog_posts = parseInt(Storage.get("N_BLOG_POSTS"));
     n_blog_posts += 1;
+    //Store the post in the storage of the contract with the key POST_postIndex
+    //The keys will have the following syntaxes: POST_1.0, POST_2.0, POST_3.0, etc.
     Storage.set(_blogKey(n_blog_posts.toString()), post);
+    //Incrementing the value of N_BLOG_POSTS in the storage of the contract
     Storage.set("N_BLOG_POSTS", n_blog_posts.toString());
 }
 
 export function deletePost(binaryArgs: StaticArray<u8>): void {
     const argsDeser = new Args(binaryArgs);
+    //Get the post index from the arguments and check if it is valid (not null), the expected format is a string of the form "1.0" or "2.0" or "3.0", etc. 
     const postIndex = argsDeser
     .nextString()
     .expect('Post index argument is missing or invalid');
+    //Delete the post from the storage of the contract by setting its value to an empty string
     Storage.set(_blogKey(postIndex), "");
 }
 
+//This function is used within the contract to generate the key of a post that will be stored in the storage of the contract
 function _blogKey(postIndex: string): string {
     return "POST_" + postIndex;
 }
