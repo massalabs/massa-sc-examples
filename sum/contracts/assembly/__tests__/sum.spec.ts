c2bbd2ccb0083c58ea1b51bcdc3c550b567d939b
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-04-17 13:25:47 +0200

    fix and prettier

--- sum/contracts/assembly/__tests__/sum.spec.ts
+++ sum/contracts/assembly/__tests__/sum.spec.ts
@@ -1,28 +1,29 @@
-import { Args, bytesToI32 } from "@massalabs/as-types"
+import { Args, bytesToI32 } from '@massalabs/as-types';
 import { sum } from '../contracts/main';
 
 describe('Testing sum contract', () => {
-    test('sum 1 + 1', () => {
-        const args = new Args();
-        args.add(1);
-        args.add(1);
-        expect(bytesToI32(sum(args.serialize()))).toStrictEqual(2);
-    });
+  test('sum 1 + 1', () => {
+    const args = new Args();
+    args.add(1);
+    args.add(1);
+    expect(bytesToI32(sum(args.serialize()))).toStrictEqual(2);
+  });
 
-    test('sum 2 - 1', () => {
-        const args = new Args();
-        args.add(2);
-        args.add(-1);
-        expect(bytesToI32(sum(args.serialize()))).toStrictEqual(1);
-    });
+  test('sum 2 - 1', () => {
+    const args = new Args();
+    args.add(2);
+    args.add(-1);
+    expect(bytesToI32(sum(args.serialize()))).toStrictEqual(1);
+  });
 
-    // The message error message is correctly displayed in the console but the test fails because it does not throw an error
-    test('sum 1 + abc', () => {
-        expect(() : void => {
-            const args = new Args();
-            args.add(1);
-            args.add("abc" as string);
-            sum(args.serialize())
-        }).toThrow("Argument b is missing or invalid");
-    });
+  // The message error message is correctly displayed in the console but the test fails because
+  // it does not throw an error
+  test('sum 1 + abc', () => {
+    expect((): void => {
+      const args = new Args();
+      args.add(1);
+      args.add('abc' as string);
+      sum(args.serialize());
+    }).toThrow('Argument b is missing or invalid');
+  });
 });
