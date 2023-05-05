1c60b6d3ae53a61b1bad821a24301afff566533e
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-05-05 09:58:49 +0200

    Update games/tictactoe/tictactoe-contract/assembly/__tests__/tictactoe.spec.ts
    Co-authored-by: Senji888 <44082144+Ben-Rey@users.noreply.github.com>

--- games/tictactoe/tictactoe-contract/assembly/__tests__/tictactoe.spec.ts
+++ games/tictactoe/tictactoe-contract/assembly/__tests__/tictactoe.spec.ts
@@ -18,20 +18,21 @@ describe('Check tictactoe', () => {
     Storage.set('gameState', 'n,n,n,n,n,n,n,n,n');
     Storage.set('gameWinner', 'n');
 
-    const args = new Args().add(0 as u32).serialize();
+    const args = new Args().add(0).serialize();
     play(args);
 
-    const args2 = new Args().add(8 as u32).serialize();
+    const args2 = new Args().add(8).serialize();
     play(args2);
 
-    const args3 = new Args().add(1 as u32).serialize();
+    const args3 = new Args().add(1).serialize();
     play(args3);
 
-    const args4 = new Args().add(7 as u32).serialize();
+    const args4 = new Args().add(7).serialize();
     play(args4);
 
-    const args5 = new Args().add(2 as u32).serialize();
+    const args5 = new Args().add(2).serialize();
     play(args5); // X should win here
+```u32
 
     expect(Storage.get('gameWinner').toString()).toBe('X');
   });
