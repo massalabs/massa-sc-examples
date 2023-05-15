31502e86145226179aee60ea47c3f3aa2c016951
unknown <adriancik01@gmail.com>
2023-05-15 16:53:36 +0200

    added title

--- pixel_war/front/src/App.tsx
+++ pixel_war/front/src/App.tsx
@@ -6,6 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className="App-content">
+        <h1>Massa Pixel War</h1>
         <PixelWar />
       </div>
     </div>
