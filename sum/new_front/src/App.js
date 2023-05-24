186b1bd25ae3f9883785c4cfed200cf44dfc7b8b
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-05-24 12:43:15 +0200

    add footer

--- sum/new_front/src/App.js
+++ sum/new_front/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Navbar from "./components/navbar";
 import Body from "./components/body";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Body />
+      <Footer />
     </div>
   );
 }
---
+++ sum/new_front/src/components/footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../css/styleSheet.css";
+
+function Footer() {
+    return (
+        <footer className="footerContent">
+            <a href="https://github.com/massalabs/massa-sc-examples" target="_blank" rel="noreferrer">
+                Repository
+            </a>
+            |
+            <a href="https://massa.net/" target="_blank" rel="noreferrer">
+                Massa Website
+            </a>
+            |
+            <a href="https://docs.massa.net/en/latest/" target="_blank" rel="noreferrer">
+                Documentation
+            </a>
+        </footer>
+    );
+}
+
+export default Footer;
