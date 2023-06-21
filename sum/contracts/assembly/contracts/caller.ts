a38c7b7fa6030c22413ee2e8dbfe439b2ff48d59
0xSwapFeeder <tristan.gremont@epitech.eu>
2023-06-21 15:29:21 +0200

    feat(sum-example): updated using massaExport

--- sum/contracts/assembly/contracts/caller.ts
+++ sum/contracts/assembly/contracts/caller.ts
@@ -9,22 +9,17 @@ import {
 /**
  * This function is meant to be called only one time: when the contract is deployed.
  */
-export function constructor(binaryArgs: StaticArray<u8>): StaticArray<u8> {
+@massaExport()
+export function constructor(addr: string): void {
   // This line is important. It ensures that this function can't be called in the future.
   // If you remove this check, someone could call your constructor function and reset your smart contract.
-  if (!callerHasWriteAccess()) {
-    return [];
-  }
-  main(binaryArgs);
-  return [];
+  if (!callerHasWriteAccess()) return;
+  main(addr);
 }
 
-export function main(binaryArgs: StaticArray<u8>): StaticArray<u8> {
-  const address = new Address(
-    new Args(binaryArgs)
-      .nextString()
-      .expect('Address argument is missing or invalid'),
-  );
+@massaExport()
+export function main(addr: string): i32 {
+  const address = new Address(addr);
   const binaryResult = call(
     address,
     'sum',
@@ -33,5 +28,5 @@ export function main(binaryArgs: StaticArray<u8>): StaticArray<u8> {
   );
   const result = bytesToI32(binaryResult);
   generateEvent('result from the contract ' + result.toString());
-  return binaryResult;
+  return result;
 }
