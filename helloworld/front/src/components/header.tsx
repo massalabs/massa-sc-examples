3c1d19d0646f919a460f6c9d4243373d3c00070b
Ben <br@massa.net>
2023-07-04 23:07:19 +0200

    clean header

--- helloworld/front/src/components/header.tsx
+++ helloworld/front/src/components/header.tsx
@@ -2,10 +2,11 @@ import { MASSA_EXEMPLE } from "../const";
 
 function Header() {
     return (
-        <header className=" text-f-primary">
-            {/* Title and quick presentation of the smart contract related to this front end */}
+        <header className="text-f-primary w-full flex-col content-center">
             <h1 className="mas-banner my-8">{MASSA_EXEMPLE.TITLE}</h1>
-            <p className="mas-subtitle my-8">{MASSA_EXEMPLE.DESCRIPTION}</p>
+            <p className="mas-subtitle my-8 mx-auto w-2/3">
+                {MASSA_EXEMPLE.DESCRIPTION}
+            </p>
         </header>
     );
 }
