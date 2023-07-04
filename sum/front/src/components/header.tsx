a57012c418744533b394f5d0b593953f7cbd95e1
Ben <br@massa.net>
2023-07-04 09:09:31 +0200

    move title to header

--- sum/front/src/components/header.tsx
+++ sum/front/src/components/header.tsx
@@ -1,7 +1,13 @@
+import { MASSA_EXEMPLE } from "../const";
 import "../css/styleSheet.css";
 
 function Header() {
-    return <header></header>;
+    return (
+        <header>
+            <h1 className="headerTitle">{MASSA_EXEMPLE.TITLE}</h1>
+            <p className="headerSubTitle">{MASSA_EXEMPLE.DESCRIPTION}</p>
+        </header>
+    );
 }
 
 export default Header;
