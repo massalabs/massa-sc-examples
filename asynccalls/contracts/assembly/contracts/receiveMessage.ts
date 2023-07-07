27cd2d13caa772617767da6fae71625c95a557ba
lilyanB <l.bastien@massa.net>
2023-07-07 11:09:20 +0200

    update async_calls contracts

--- asynccalls/contracts/assembly/contracts/receiveMessage.ts
+++ asynccalls/contracts/assembly/contracts/receiveMessage.ts
@@ -1,12 +1,13 @@
-import { print } from '@massalabs/massa-as-sdk';
+import { print, generateEvent } from '@massalabs/massa-as-sdk';
 import { Args } from '@massalabs/as-types';
 /** ***********************
  * Smart contract containing a message handler function
  **/
-import { generateEvent } from '@massalabs/massa-as-sdk';
 
 export function receive(data: StaticArray<u8>): StaticArray<u8> {
-  const msg: string = new Args(data).nextString().expect('msg argument is missing or invalid');
+  const msg: string = new Args(data)
+    .nextString()
+    .expect('msg argument is missing or invalid');
   const response: string = `message received: ${msg}`;
   generateEvent(response);
   print(response);
---
+++ asynccalls/contracts/assembly/contracts/sendHelloAsyncMessage.ts
@@ -0,0 +1,73 @@
+import { Args } from '@massalabs/as-types';
+/** ***********************
+ *
+ * SendHelloAsyncMessage sends an asynchronous message
+ * to a previous deployed contract containing a message handler function
+ **/
+
+import {
+  sendMessage,
+  callerHasWriteAccess,
+  Address,
+} from '@massalabs/massa-as-sdk';
+import { currentPeriod } from '@massalabs/massa-as-sdk/assembly/std/context';
+
+/**
+
+ * This function is meant to be called only one time: when the contract is deployed.
+
+ */
+export function constructor(binaryArgs: StaticArray<u8>): StaticArray<u8> {
+  // This line is important. It ensure that this function can't be called in the future.
+  // If you remove this check someone could call your constructor function and reset your SC.
+  if (!callerHasWriteAccess()) {
+    return [];
+  }
+  sendHelloAsyncMessage(binaryArgs);
+  return [];
+}
+
+/**
+
+ * @param binaryArgs - The address of the receive message contract encoded with `Args`
+
+ * @returns empty array
+
+ */
+export function sendHelloAsyncMessage(
+  binaryArgs: StaticArray<u8>,
+): StaticArray<u8> {
+  // Setup the 'message' we will send to our deployed SC
+  const functionName = 'receive';
+  const currentPeriodStart = currentPeriod();
+  const validityStartPeriod = currentPeriodStart + 1;
+  const validityStartThread = 13 as u8;
+  const validityEndPeriod = currentPeriodStart + 20;
+  const validityEndThread = 13 as u8;
+  const maxGas = 1_000_000_000; // gas for smart contract execution.
+  const rawFee = 0;
+  const coins = 100; // coins that can be used inside SC.
+  const msg = new Args().add('hello my good friend!').serialize();
+
+  const args = new Args(binaryArgs);
+
+  const address = new Address(
+    args.nextString().expect('Address argument is missing or invalid'),
+  );
+
+  // Send the message
+  sendMessage(
+    address,
+    functionName,
+    validityStartPeriod,
+    validityStartThread,
+    validityEndPeriod,
+    validityEndThread,
+    maxGas,
+    rawFee,
+    coins,
+    msg,
+  );
+
+  return [];
+}
