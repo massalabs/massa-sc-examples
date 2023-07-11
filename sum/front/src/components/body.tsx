daf462355b26a45dc46b37318dd02d8d5c37f2bc
Ben <br@massa.net>
2023-07-11 10:18:16 +0200

    cean body

--- sum/front/src/components/body.tsx
+++ sum/front/src/components/body.tsx
@@ -3,42 +3,40 @@ import ContractInteraction from "./contractInteraction";
 import { ProviderService } from "../interfaces/ProviderService";
 import AccountCreation from "./accountCreation";
 
-export default function Body(provider: ProviderService) {
-    const {
-        connect,
-        createAccount,
-        connected,
-        account,
-        balance,
-        errorMessage,
-    } = provider;
-
-    return (
-        <div className="body">
-            <div className="bodyContent">
-                {!connected && (
-                    <>
-                        <button className="bodyButton" onClick={connect}>
-                            Connect to Massa Station
-                        </button>
-                    </>
-                )}
-                {connected && !account && (
-                    <AccountCreation createAccount={createAccount} />
-                )}
-                {connected && account && (
-                    <>
-                        <AccountInformation
-                            accountAddress={account.address()}
-                            accountName={account.name()}
-                            balance={balance.finalBalance}
-                        />
-                        <br></br>
-                        <ContractInteraction account={account} />
-                    </>
-                )}
-            </div>
-            <div className="mas-h2 text-red-500">{errorMessage}</div>
+const Body = ({
+    connect,
+    createAccount,
+    connected,
+    account,
+    balance,
+    errorMessage,
+}: ProviderService) => (
+    <div className="body">
+        <div className="bodyContent">
+            {!connected && (
+                <>
+                    <button className="bodyButton" onClick={connect}>
+                        Connect to Massa Station
+                    </button>
+                </>
+            )}
+            {connected && !account && (
+                <AccountCreation createAccount={createAccount} />
+            )}
+            {connected && account && (
+                <>
+                    <AccountInformation
+                        accountAddress={account.address()}
+                        accountName={account.name()}
+                        balance={balance.finalBalance}
+                    />
+                    <br></br>
+                    <ContractInteraction account={account} />
+                </>
+            )}
         </div>
-    );
-}
+        <div className="mas-h2 text-red-500">{errorMessage}</div>
+    </div>
+);
+
+export default Body;
