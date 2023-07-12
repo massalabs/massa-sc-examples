f6ad6bc679ae1a04bf2c689f8c1a5aab1003e46d
unknown <adriancik01@gmail.com>
2023-07-12 14:36:55 +0200

    use massaExport async-calls

--- asynccalls/contracts/assembly/contracts/receiveMessage.ts
+++ asynccalls/contracts/assembly/contracts/receiveMessage.ts
@@ -1,14 +1,12 @@
 import { print } from '@massalabs/massa-as-sdk';
-import { Args } from '@massalabs/as-types';
 /** ***********************
  * Smart contract containing a message handler function
  **/
 import { generateEvent } from '@massalabs/massa-as-sdk';
 
-export function receive(data: StaticArray<u8>): StaticArray<u8> {
-  const msg: string = new Args(data).nextString().expect('msg argument is missing or invalid');
+@massaExport()
+export function receive(msg: string): void {
   const response: string = `message received: ${msg}`;
   generateEvent(response);
   print(response);
-  return [];
 }
---
+++ asynccalls/contracts/assembly/contracts/sendHelloAsyncMessage.ts
@@ -0,0 +1,65 @@
+import { Args } from '@massalabs/as-types';
+/** ***********************
+ *
+ * SendHelloAsyncMessage sends an asynchronous message
+ * to a previous deployed contract containing a message handler function
+ **/
+
+import {
+  sendMessage,
+  callerHasWriteAccess,
+  Address,
+} from '@massalabs/massa-as-sdk';
+import { currentPeriod } from '@massalabs/massa-as-sdk/assembly/std/context';
+
+/**
+
+ * This function is meant to be called only one time: when the contract is deployed.
+
+ */
+export function constructor(addr: string): void {
+  // This line is important. It ensure that this function can't be called in the future.
+  // If you remove this check someone could call your constructor function and reset your SC.
+  if (!callerHasWriteAccess()) {
+    return;
+  }
+  sendHelloAsyncMessage(addr);
+}
+
+/**
+
+ * @param addr - The address of the receive message contract
+
+ * @returns empty array
+
+ */
+export function sendHelloAsyncMessage(
+  addr: string): void {
+  // Setup the 'message' we will send to our deployed SC
+  const functionName = 'receive';
+  const currentPeriodStart = currentPeriod();
+  const validityStartPeriod = currentPeriodStart + 1;
+  const validityStartThread = 13 as u8;
+  const validityEndPeriod = currentPeriodStart + 20;
+  const validityEndThread = 13 as u8;
+  const maxGas = 1_000_000_000; // gas for smart contract execution.
+  const rawFee = 0;
+  const coins = 100; // coins that can be used inside SC.
+  const msg = new Args().add('hello my good friend!').serialize();
+
+  const address = new Address(addr);
+
+  // Send the message
+  sendMessage(
+    address,
+    functionName,
+    validityStartPeriod,
+    validityStartThread,
+    validityEndPeriod,
+    validityEndThread,
+    maxGas,
+    rawFee,
+    coins,
+    msg,
+  );
+}
