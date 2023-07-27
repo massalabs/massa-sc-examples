8e6e4d4aeb9620a29e8aca26d0e6e861f217f4a8
unknown <adriancik01@gmail.com>
2023-07-27 17:47:29 +0200

    fix formatting

--- blog/contracts/assembly/contracts/main.ts
+++ blog/contracts/assembly/contracts/main.ts
@@ -1,5 +1,5 @@
-import { Storage, Context, generateEvent } from "@massalabs/massa-as-sdk";
-import { Args, stringToBytes } from "@massalabs/as-types";
+import { Storage, Context, generateEvent } from '@massalabs/massa-as-sdk';
+import { Args, stringToBytes } from '@massalabs/as-types';
 
 /**
  * This function is meant to be called only one time: when the contract is deployed.
@@ -14,26 +14,26 @@ export function constructor(_: StaticArray<u8>): void {
   }
   const nBlogPosts = 0;
   // Set the initial value of N_BLOG_POSTS to 0 in the storage of the contract
-  Storage.set("N_BLOG_POSTS", nBlogPosts.toString());
+  Storage.set('N_BLOG_POSTS', nBlogPosts.toString());
   // This function is used to emit an event to the blockchain
-  generateEvent("Blog initiated");
+  generateEvent('Blog initiated');
 }
 
 export function post(_args: StaticArray<u8>): string {
   const args = new Args(_args);
   // Get the post from the arguments and check if it is valid (not null)
-  const post = args.nextString().expect("Post argument is missing or invalid");
+  const post = args.nextString().expect('Post argument is missing or invalid');
 
   let postLastIndex = 0;
-  if (Storage.get<string>("N_BLOG_POSTS") !== "") {
-    postLastIndex = parseInt(Storage.get("N_BLOG_POSTS")) as i32;
+  if (Storage.get<string>('N_BLOG_POSTS') !== '') {
+    postLastIndex = parseInt(Storage.get('N_BLOG_POSTS')) as i32;
   }
   postLastIndex += 1;
   // Store the post in the storage of the contract with the key POST_postIndex
   // The keys will have the following syntaxes: POST_1, POST_2, POST_3, etc.
   Storage.set(blogKey(postLastIndex.toString()), post);
   // Incrementing the value of N_BLOG_POSTS in the storage of the contract
-  Storage.set("N_BLOG_POSTS", postLastIndex.toString());
+  Storage.set('N_BLOG_POSTS', postLastIndex.toString());
   return post;
 }
 
@@ -42,36 +42,36 @@ export function deletePost(_args: StaticArray<u8>): void {
   // Get the post index from the arguments and check if it is valid, we expect a string of the form "1.0" or "2.0", etc.
   const postIndex = args
     .nextString()
-    .expect("Post index argument is missing or invalid");
+    .expect('Post index argument is missing or invalid');
   // Delete the post from the storage of the contract by setting its value to an empty string
-  Storage.set(blogKey(postIndex), "");
+  Storage.set(blogKey(postIndex), '');
 }
 
 // This function is used within the contract to generate the key of a post that will be stored in the storage
 export function blogKey(postIndex: string): string {
-  return "POST_" + postIndex;
+  return 'POST_' + postIndex;
 }
 
 export function getPosts(): StaticArray<u8> {
-  let posts = "";
+  let posts = '';
   let postLastIndex = 0;
 
   // Check if N_BLOG_POSTS exists in the storage and if it does, retrieve the value
-  if (Storage.get<string>("N_BLOG_POSTS") !== "") {
-    postLastIndex = parseInt(Storage.get("N_BLOG_POSTS")) as i32;
+  if (Storage.get<string>('N_BLOG_POSTS') !== '') {
+    postLastIndex = parseInt(Storage.get('N_BLOG_POSTS')) as i32;
   }
 
   // Loop through all posts by their indices
   for (let i = 1; i <= postLastIndex; i++) {
     const post = Storage.get(blogKey(i.toString()));
-    if (post !== "") {
+    if (post !== '') {
       // Check if post exists
-      if (posts !== "") posts += ","; // If it's not the first post, prepend comma
+      if (posts !== '') posts += ','; // If it's not the first post, prepend comma
       posts += post; // Concatenate posts
     }
   }
 
-  posts = "[" + posts + "]"; // Wrap posts string with array brackets
+  posts = '[' + posts + ']'; // Wrap posts string with array brackets
 
   return stringToBytes(posts); // Return all posts as an array string
 }
