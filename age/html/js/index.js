46d397b4a2c12e7d04b21b1e7c0b6e843dd16000
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-07-28 16:23:46 +0200

    import ok

--- age/html/js/index.js
+++ age/html/js/index.js
@@ -1,15 +1,18 @@
 let massa = window.massa;
+let wallet = window.wallet;
 
 let client = null;
 let account = null;
+let ageResult = null;
 let Args = massa.Args;
 
-const sc_addr = "AS1284LtJxDNyYTMLioPtbnsF3h3xAXMFnDF1kBrKBjN4WDSdbzsw";
+const CONTRACT_ADDRESS =
+  "AS1284LtJxDNyYTMLioPtbnsF3h3xAXMFnDF1kBrKBjN4WDSdbzsw";
 
-// initialize a testnet client
+/* Functions */
 const initializeClient = async () => {
   try {
-    let provider = (await massa.providers(true, 10000))[0];
+    let provider = (await wallet.providers(true, 10000))[0];
     let accounts = await provider.accounts();
     if (accounts.length === 0) {
       console.log("no accounts found");
@@ -25,24 +28,27 @@ const initializeClient = async () => {
   }
 };
 
-const callGetAge = async () => {
-    try {
-      if (!account || !client) {
-        return;
-      }
-      console.log("calling getAge");
-      let res = await client.smartContracts().readSmartContract({
-        maxGas: BigInt(1000000),
-        targetAddress: CONTRACT_ADDRESS,
-        targetFunction: "getAge",
-        parameter: new Args().addString(inputName).serialize(),
-      });
-
-      setAgeResult(bytesToU32(res.returnValue));
-    } catch (error) {
-      console.error(error);
+async function getAge() {
+  try {
+    console.log("calling getAge");
+    if (!account || !client) {
+      console.log("no account or client");
+      await initializeClient();
+      return;
     }
+    console.log("calling getAge");
+    let res = await client.smartContracts().readSmartContract({
+      maxGas: BigInt(1000000),
+      targetAddress: CONTRACT_ADDRESS,
+      targetFunction: "getAge",
+      parameter: new Args().addString("alice").serialize(),
+    });
+    ageResult = massa.bytesToU32(res.returnValue);
+    console.log("ageResult: " + ageResult);
+  } catch (error) {
+    console.error(error);
   }
+}
 
 function funcSetAge(number) {
   let args = new Args();
@@ -59,25 +65,6 @@ function funcSetAge(number) {
         coins: 0,
         targetAddress: sc_addr,
         functionName: "change_age",
-        parameter: args.serialize(),
-      })
-      .then((res) => {
-        console.log(res);
-      });
-  }
-}
-
-function initialize() {
-  let args = new Args();
-  if (client) {
-    client
-      .smartContracts()
-      .callSmartContract({
-        fee: 0,
-        maxGas: 1000000,
-        coins: 10_000_000_000,
-        targetAddress: sc_addr,
-        functionName: "initialize",
         parameter: args.serialize(),
       })
       .then((res) => {
