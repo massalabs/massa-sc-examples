1a768e18477312bb539bec8e52e2c8be77bf8195
unknown <adriancik01@gmail.com>
2023-08-14 15:30:37 +0200

    overhaul front end to keep all the content in App.tsx

--- autonomousprice/front/src/const/index.ts
+++
@@ -1,8 +0,0 @@
-export const MASSA_EXEMPLE = {
-    TITLE: "Massa Autonomous Price",
-    DESCRIPTION: "This is a simple dApp showcasing Massa's Autonomous Smart Contract feature.",
-    TAB: "Massa - Autonomous Price example",
-};
-
-export const CONTRACT_ADDRESS =
-    "AS12tdw4onaZqFkSTHqSFWaJZxSQST2EWU7xQoXmFETDkSEiZPLjs";
