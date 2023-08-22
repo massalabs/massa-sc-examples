e7e352d185fc2ddc94a93264686780887f4c7ec0
Ben <br@massa.net>
2023-08-22 10:19:27 +0200

    switch wallet example

---
+++ wallet-provider/next.config.mts
@@ -0,0 +1,4 @@
+module.exports = {
+  swcMinify: false,
+  swcLoader: false,
+};
---
+++ wallet-provider/pages/index.tsx
@@ -0,0 +1,241 @@
+"use client"; // This is a client component 👈🏽
+import { useEffect, useState } from "react";
+import { IAccount, IProvider, providers } from "@massalabs/wallet-provider";
+import pollAsyncEvents from "../utils/pollAsyncEvent";
+import {
+  Args,
+  toMAS,
+  Client,
+  IClient,
+  ClientFactory,
+  DefaultProviderUrls,
+} from "@massalabs/massa-web3";
+
+import {
+  PublicKey,
+  SecretKey,
+} from "@massalabs/massa-web3/dist/esm/utils/keyAndAddresses";
+
+const CONTRACT_ADDRESS = "AS1MAtScFwncd19rktPEqHQ8D3ZicoDGKm7nXyt1AaJFfzAx3Lbg";
+
+const getWallet = async (walletName: string) => {
+  const wallets = await providers();
+  const _wallet = wallets.find((wallet) => {
+    if (wallet.name() === walletName) {
+      return wallet;
+    }
+  });
+  return _wallet;
+};
+
+export default function IndexPage() {
+  const [wallet, setWallet] = useState<IProvider | null>(null);
+  const [accounts, setAccounts] = useState<IAccount[]>([]);
+  const [client, setClient] = useState<Client | null>(null);
+  const [clientWeb3, setClientWeb3] = useState<Client | null>(null);
+  const [selectedAccount, setSelectedAccount] = useState<IAccount | null>(null);
+  const [finalBalance, setFinalBalance] = useState<string | null>(null);
+  const [opId, setOpId] = useState<string | null>(null);
+  const [clientWeb3PublicKey, setClientWeb3PublicKey] = useState<string | null>(
+    null
+  );
+
+  const initAccount = async (wallet: IProvider) => {
+    const accounts: IAccount[] = await wallet.accounts();
+    if (!accounts.length) {
+      console.log("Error: You need to create an account");
+    }
+    return accounts;
+  };
+
+  useEffect(() => {
+    init("MASSASTATION");
+  }, []);
+
+  const initClientWallet = async (wallet: IProvider, account: IAccount) => {
+    return await ClientFactory.fromWalletProvider(wallet, account);
+  };
+
+  const initClientWeb3 = async (wallet: IProvider, account: IAccount) => {
+    const secret = "S1NA786im4CFL5cHSmsGkGZFEPxqvgaRP8HXyThQSsVnWj4tR7d";
+    const secretKey = new SecretKey(secret);
+    const pub: PublicKey = await secretKey.getPublicKey();
+    setClientWeb3PublicKey(pub.base58Encode);
+    return await ClientFactory.createDefaultClient(
+      DefaultProviderUrls.BUILDNET,
+      false,
+      {
+        address: "AU1wN8rn4SkwYSTDF3dHFY4U28KtsqKL1NnEjDZhHnHEy6cEQm53",
+        publicKey: pub.base58Encode,
+        secretKey: secret,
+      }
+    );
+  };
+
+  const init = async (chosenWallet: string) => {
+    const wallet = await getWallet(chosenWallet);
+    if (!wallet) return;
+    setWallet(wallet);
+    const accounts: IAccount[] = await initAccount(wallet);
+    if (!accounts.length) return;
+    setAccounts(accounts);
+
+    const account = accounts[0];
+    setSelectedAccount(account);
+
+    const client = await initClientWallet(wallet, account);
+    setClient(client);
+
+    const clientWeb3 = await initClientWeb3(wallet, account);
+    setClientWeb3(clientWeb3);
+  };
+
+  const getBalance = async (client: IClient, accountAddress: string) => {
+    if (!client) return;
+
+    const balance = await client.wallet().getAccountBalance(accountAddress);
+
+    setFinalBalance(balance?.final.toString() || null);
+
+    return balance?.final.toString();
+  };
+
+  const logSelectedAccount = async () => {
+    console.log("selectedAccount", await selectedAccount?.balance());
+  };
+
+  async function storeMessage(_client: Client) {
+    try {
+      const result = await _client.smartContracts().callSmartContract({
+        fee: BigInt(1000),
+        maxGas: BigInt(1000000),
+        coins: BigInt(0),
+        targetAddress: CONTRACT_ADDRESS,
+        functionName: "storeMessage",
+        parameter: new Args().addString("Hello World!"),
+      });
+      setOpId(result);
+      pollAsyncEvents(_client, result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function signMessage(_client: Client) {
+    console.log("------------------------------------ signMessage");
+    try {
+      const result = await _client
+        .wallet()
+        .signMessage("Test", _client.wallet().getBaseAccount().address() || "");
+      console.log("result", result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    init("Bearby");
+  }, []);
+
+  useEffect(() => {
+    const accountAddress = selectedAccount?.address();
+    if (!accountAddress || !client) return;
+    getBalance(client, accountAddress);
+  }, [client, selectedAccount]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Massa Web3</h1>
+        <p className="text-xl mt-4">
+          This is a client component, it will only work on the browser
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-2xl font-bold">Wallet</h2>
+        <p className="text-xl mt-4">
+          {wallet ? wallet.name() : "No wallet found"}
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-2xl font-bold">Account</h2>
+        <p className="text-xl mt-4">
+          {selectedAccount?.address() || "No account found"}
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-2xl font-bold">Balance</h2>
+        {/* {finalBalance && (
+           <p className="text-xl mt-4">{toMAS(finalBalance).toString()}</p>
+        )} */}
+      </div>
+      <div className="flex items-center gap-5 justify-center border p-4 rounded-md">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={async () => {
+            await init("MASSASTATION");
+          }}
+        >
+          Connect Massa Station
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={async () => {
+            await init("Bearby");
+          }}
+        >
+          Connect Bearby
+        </button>
+      </div>
+      {/* Button to store message */}
+      <div className="flex gap-5 items-center justify-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={async () => {
+            if (!clientWeb3) return;
+            await storeMessage(clientWeb3);
+          }}
+        >
+          Store message Web3
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+          onClick={async () => {
+            if (!client) return;
+            await storeMessage(client);
+          }}
+        >
+          Store message Wallet Provider
+        </button>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+          onClick={async () => {
+            if (!client) return;
+            await signMessage(clientWeb3);
+          }}
+        >
+          Sign message Web3
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+          onClick={async () => {
+            if (!client) return;
+            await signMessage(client);
+          }}
+        >
+          Sign message Wallet Provider
+        </button>
+      </div>
+      {/* Button to call logSelectedAccount */}
+      <div className="flex gap-5 items-center justify-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={logSelectedAccount}
+        >
+          Log selected account
+        </button>
+      </div>
+    </main>
+  );
+}
