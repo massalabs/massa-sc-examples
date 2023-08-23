ad2a716a94a7d534b9f02f9623413a707227c3d5
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-08-23 08:22:08 +0200

    update contract and add protobuf

--- sum/contracts/src/deploy.ts
+++ sum/contracts/src/deploy.ts
@@ -3,58 +3,56 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { deploySC, WalletClient, ISCData } from '@massalabs/massa-sc-deployer';
-import { Args, IEvent, fromMAS } from '@massalabs/massa-web3';
+import { Args, fromMAS } from '@massalabs/massa-web3';
 
+// Load .env file content into process.env
 dotenv.config();
 
+// Get the URL for a public JSON RPC API endpoint from the environment variables
 const publicApi = process.env.JSON_RPC_URL_PUBLIC;
 if (!publicApi) {
   throw new Error('Missing JSON_RPC_URL_PUBLIC in .env file');
 }
-const privKey = process.env.WALLET_PRIVATE_KEY;
-if (!privKey) {
-  throw new Error('Missing WALLET_PRIVATE_KEY in .env file');
+
+// Get the secret key for the wallet to be used for the deployment from the environment variables
+const secretKey = process.env.WALLET_SECRET_KEY;
+if (!secretKey) {
+  throw new Error('Missing WALLET_SECRET_KEY in .env file');
 }
 
-const deployerAccount = await WalletClient.getAccountFromSecretKey(privKey);
+// Create an account using the private key
+const deployerAccount = await WalletClient.getAccountFromSecretKey(secretKey);
 
+// Obtain the current file name and directory paths
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(path.dirname(__filename));
 
+/**
+ * Deploy one or more smart contracts.
+ *
+ * @remarks
+ * Multiple smart contracts can be deployed by adding more objects to the array.
+ * In this example one contract located at 'build/main.wasm' is deployed with
+ * 0.1 MASSA and an argument 'Test'.
+ *
+ * After all deployments, it terminates the process.
+ */
 (async () => {
-  let deployed = await deploySC(
-    publicApi,
-    deployerAccount,
-    [
-      {
-        data: readFileSync(path.join(__dirname, 'build', 'sum.wasm')),
-        coins: fromMAS(0.5),
-      } as ISCData,
-    ],
-    0n,
-    4_200_000_000n,
-    true,
-  );
-  const data = (deployed.events?.find((e) => e.data) as IEvent).data;
-  const address = data
-    .split('Contract deployed at address: ')[1]
-    .trim()
-    .replace(' ', '');
-
   await deploySC(
-    publicApi,
-    deployerAccount,
+    publicApi, // JSON RPC URL
+    deployerAccount, // account deploying the smart contract(s)
     [
       {
-        data: readFileSync(path.join(__dirname, 'build', 'run.wasm')),
-        coins: fromMAS(0.5),
-        args: new Args().addString(address),
+        data: readFileSync(path.join(__dirname, 'build', 'sum.wasm')), // smart contract bytecode
+        coins: fromMAS(0.1), // coins for deployment
+        args: new Args(), // arguments for deployment
+        protoPaths: ['./build/lastResultHelper.proto', './build/sumHelper.proto'], // proto files for deployment
       } as ISCData,
+      // Additional smart contracts can be added here for deployment
     ],
-    0n,
-    4_200_000_000n,
-    true,
+    0n, // fees for deployment
+    4_200_000_000n, // max gas for deployment
+    true, // if true, waits for the first event before returning
   );
-  process.exit(0);
+  process.exit(0); // terminate the process after deployment(s)
 })();
