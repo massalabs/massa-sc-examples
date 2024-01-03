550ca88ef63e0e62ca953bdd8539964461430309
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2024-01-03 20:28:24 +0100

    Update autonomousprice/contracts

--- autonomousprice/contracts/assembly/contracts/autonomousprice.ts
+++ autonomousprice/contracts/assembly/contracts/autonomousprice.ts
@@ -10,28 +10,32 @@ import { u64ToBytes } from '@massalabs/as-types';
 import { currentPeriod } from '@massalabs/massa-as-sdk/assembly/std/context';
 
 const PRICE_KEY = 'PRICE_KEY';
-const INIT_PRICE: u64 = 10000;
+const INIT_PRICE = 100000 as u64;
+const MAX_CHANGE = 20;
 
 export function constructor(_: StaticArray<u8>): StaticArray<u8> {
   // This line is important. It ensure that this function can't be called in the future.
   // If you remove this check someone could call your constructor function and reset your SC.
   assert(callerHasWriteAccess(), 'Caller is not allowed');
 
-  setPrice([]);
+  Storage.set(PRICE_KEY, INIT_PRICE.toString());
+  generateEvent(`Set initial price to ${INIT_PRICE.toString()}`);
 
+  sendFutureOperation();
   return [];
 }
 
-function sendFuturOperation(): void {
-  const functionName = 'setPrice';
+export function sendFutureOperation(): void {
   const address = Context.callee();
+  const functionName = 'updatePrice';
   const validityStartPeriod = currentPeriod() + 1;
   const validityStartThread = 0 as u8;
   const validityEndPeriod = validityStartPeriod;
   const validityEndThread = 31 as u8;
-  const maxGas = 1_000_000_000; // gas for smart contract execution
+
+  const maxGas = 500_000_000; // gas for smart contract execution
   const rawFee = 0;
-  const coins = 0; // coins that can be used inside SC
+  const coins = 0;
 
   // Send the message
   sendMessage(
@@ -52,46 +56,29 @@ function sendFuturOperation(): void {
   );
 }
 
-// Generate a random price change of +/- 5%.
-function generateRandomIncrease(base: u64): u64 {
-  const randomInt = unsafeRandom();
-  const increasePercent = (abs(randomInt) % 10) - 5;
-  const increase = ((base as i64) * increasePercent) / 100;
+export function updatePrice(_: StaticArray<u8>): StaticArray<u8> {
+  const currentPrice = u64.parse(Storage.get(PRICE_KEY));
 
-  if (increase < 0 && base <= (abs(increase) as u64)) {
-    return 0;
-  }
-  return base + increase;
-}
+  const priceChange = (unsafeRandom() % (2 * MAX_CHANGE + 1)) - MAX_CHANGE;
 
-export function setPrice(_: StaticArray<u8>): StaticArray<u8> {
-  assert(callerHasWriteAccess(), 'Caller is not allowed');
-
-  let currentPrice: u64;
-  if (!Storage.has(PRICE_KEY)) {
-    // Set initial autonomousprice price
-    generateEvent(`Set initial price to ${INIT_PRICE.toString()}`);
-    Storage.set(PRICE_KEY, INIT_PRICE.toString());
-    currentPrice = INIT_PRICE;
-  } else {
-    currentPrice = u64.parse(Storage.get(PRICE_KEY));
-  }
-
-  const newPrice = generateRandomIncrease(currentPrice);
+  const newPrice = currentPrice + priceChange;
 
   Storage.set(PRICE_KEY, newPrice.toString());
+
   generateEvent(`🎉 Price updated: ${newPrice.toString()}`);
 
-  sendFuturOperation();
+  sendFutureOperation();
 
   return u64ToBytes(newPrice);
 }
 
 export function getPrice(_: StaticArray<u8>): StaticArray<u8> {
-  assert(!Storage.has(PRICE_KEY), 'Price is not set');
+  assert(Storage.has(PRICE_KEY), 'Price is not set');
 
   const price = u64.parse(Storage.get(PRICE_KEY));
-  generateEvent(`current price is ${price.toString()}`);
+  generateEvent(
+    `current price is ${price.toString()} ${Storage.has(PRICE_KEY).toString()}`,
+  );
 
   return u64ToBytes(price);
 }
